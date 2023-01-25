f78a37827fb9358ec87d5939f3bd2fff6cb03ce8
Gabrielhephzibah <gabrielhephzibah24@gmail.com>
2023-01-25 14:17:30 +0100

    create headlines and headlineDetails fragment and xml

--- app/src/main/java/com/example/topheadlinesapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/topheadlinesapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.topheadlinesapp
+package com.example.topheadlinesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.topheadlinesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
